54575027e4707591e30bbb4bb7d3ec261663520a
hxse <narutohy@163.com>
2025-02-10 17:37:20 +0800

    update

--- copy_file.bat
+++ copy_file.bat
@@ -1,2 +1,4 @@
 xcopy /y "D:\my_repo\chameleon-quant\src\plot\bokeh_plot.py" "D:\my_repo\chameleon-fastapi\src\plot"
 xcopy /y "D:\my_repo\chameleon-quant\src\plot\bokeh_server.py" "D:\my_repo\chameleon-fastapi\src\plot"
+D: && cd "D:\my_repo\chameleon-fastapi"
+".\push git.bat"
